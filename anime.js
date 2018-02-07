ad90b51cae47393816da4fb7619334c00374313f
William Meng <willmeng@gmail.com>
2018-02-06 16:26:00 -0800

    Fix animation of Merkle Tree

--- anime.js
+++ anime.js
@@ -17,7 +17,9 @@ export const createTreeLines = (liWidth, i, iMax, row) => {
   svg.style.left = `${(sides + liWidth/3) + i/2 * 2 * (liWidth + 2 * sides)}px`; // start at 1/3 width from edge
   path.setAttribute('d', `M2 ${HEIGHT/4 + 2} v-${HEIGHT/4} h${width} 
     M${2*width} ${HEIGHT/4 + 2} v-${HEIGHT/4} h-${width}`);
-  path.classList.add('path-' + row);
+  path.classList.add(`path-${row}`);
+  path.classList.add('path');
+  path.classList.add('hidden');
   svg.appendChild(path);
   tree.insertBefore(svg, document.getElementById('merkle-row-4'));
 };
@@ -28,12 +30,120 @@ export const drawTreeLines = () => {
   const duration = 1000;
 
   anime.timeline()
-  .add({ targets: '.path-1', strokeDashoffset, easing, duration })
-  .add({ targets: '.path-2', strokeDashoffset, easing, duration })
-  .add({ targets: '.path-3', strokeDashoffset, easing, duration });
+    .add({ targets: '.path-1', strokeDashoffset, easing, duration })
+    .add({ targets: '.path-2', strokeDashoffset, easing, duration })
+    .add({ targets: '.path-3', strokeDashoffset, easing, duration });
 
   setTimeout(() => {
       document.getElementById('tut-3').classList.remove('hidden');
       document.getElementById('start-mining').classList.remove('vis-hidden');
     }, duration * 3);
 };
+
+export const animateMerkleTree = (transactions, row2Hashes, row1, row2, row3, row4) => {
+  const hash11El = document.createElement('pre'), 
+    hash12El = document.createElement('pre'),
+    hash21El = document.createElement('pre'),
+    plusSign = document.createElement('span'),
+    sha256Text = document.createElement('pre');
+  hash11El.id = 'hash-11';
+  hash12El.id = 'hash-12';
+  hash21El.id = 'hash-21';
+  plusSign.id = 'plus';
+  sha256Text.id = 'sha256-text';
+  hash11El.textContent = "transaction #1 hash\n" + transactions[0].hash;
+  hash12El.textContent = transactions[1].hash + "\ntransaction #2 hash";
+  hash21El.textContent = row2Hashes[0];
+  plusSign.textContent = '+';
+  sha256Text.textContent = 'SHA-256( SHA-256(                                 ))';
+  row1.appendChild(hash11El);
+  row1.appendChild(hash12El);
+  row3.appendChild(hash21El);
+  row3.appendChild(plusSign);
+  row3.appendChild(sha256Text);
+
+  const promise = anime.timeline()
+    .add({
+      targets: hash11El,
+      translateX: 274,
+      translateY: -287,
+      fontSize: '20px',
+      color: '#FFF',
+      opacity: 1,
+      duration: 3000,
+      easing: 'easeInOutQuad',
+    })
+    .add({
+      targets: plusSign,
+      opacity: 1,
+      duration: 1000
+    })
+    .add({
+      targets: sha256Text,
+      opacity: 1,
+      duration: 1000
+    })
+    .add({
+      targets: [hash11El, plusSign, sha256Text],
+      opacity: 0,
+      color: '#000',
+      duration: 500,
+      delay: 1000
+    })
+    .add({
+      targets: '.row-2-el',
+      opacity: 1,
+      duration: 1000,
+      delay: 4000,
+    })
+    .add({
+      targets: '.row-3-el',
+      opacity: 1,
+      duration: 1000,
+    })
+    .add({
+      targets: '.row-4-el',
+      opacity: 1,
+      duration: 1000,
+    })
+    .add({
+      targets: '.qm',
+      opacity: 1,
+      duration: 1000,
+    })
+
+  anime.timeline()
+    .add({
+      targets: hash12El,
+      translateX: 163,
+      translateY: -250,
+      fontSize: '20px',
+      color: '#FFF',
+      opacity: 1,
+      duration: 3000,
+      easing: 'easeInOutQuad',
+    })
+    .add({
+      targets: hash12El,
+      opacity: 0,
+      color: '#000',
+      duration: 500,
+      delay: 3000,
+    })
+    .add({
+      targets: hash21El,
+      opacity: { value: 1, duration: 500 },
+      translateX: { value: -218, duration: 2000, delay: 2000 },
+      translateY: { value: 94, duration: 2000, delay: 2000 },
+      fontSize: { value: '14px', duration: 2000, delay: 2000 },
+      color: { value: '#000', duration: 1000, delay: 2000 },
+      easing: 'easeInOutQuad',
+    })
+    .add({
+      targets: hash21El,
+      opacity: 0,
+      duration: 500,
+    })
+
+    return promise;
+}
